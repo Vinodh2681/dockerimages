a84a219c387864601b5d1c4788a01a10c03e6042
Vinodh2681 <108776518+Vinodh2681@users.noreply.github.com>
2022-07-06 14:41:42 +0530

    Add files via upload

---
+++ brand.conf.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'BRAND_LOGO' => './tcl_logo.png',
+    'BRAND_LOGO_SIDEBAR' => './side_bar.png',
+    'BRAND_LOGO_SIDEBAR_COMPACT' => './compact.png',
+    'BRAND_FOOTER' => '© 2022 Tata Communications. All rights reserved. TATA COMMUNICATIONS and TATA are trademarks of Tata Sons Private Limited.',
+    'BRAND_HELP_URL' => 'https://jira.tatacommunications.com/secure/RapidBoard.jspa?rapidView=1000&projectKey=OJAS&view=planning.nodetail&quickFilter=8378&issueLimit=100'
+];
+
